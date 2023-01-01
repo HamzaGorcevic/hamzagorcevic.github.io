75b4304779b7ba456352a90d24fa412b43512a0e
HamzaGorcevic <gorcevichamza76@gmail.com>
2023-01-01 16:00:01 +0100

    fixed changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -10,16 +10,12 @@ import axios from "axios";
 
 export default function Home() {
   const [quote, setQuote] = useState("");
+
   useEffect(() => {
     axios
-      .get("https://api.api-ninjas.com/v1/quotes?category=happines", {
-        headers: {
-          "X-Api-Key": "X-Api-Key=9PPstyccNokjqUydrLQZuw==G7kBDczOSVgc0ZlE",
-        },
-      })
+      .get("https://api.quotable.io/random?tags=technology")
       .then((response) => {
-        // If request is good...
-        console.log(response.data);
+        setQuote(response.data);
       })
       .catch((error) => {
         console.log("error " + error);
@@ -30,60 +26,68 @@ export default function Home() {
     <Box
       component={"main"}
       display={"flex"}
-      flexDirection={{ xs: "column", md: "row" }}
+      flexDirection={{ xs: "column", md: "column" }}
       alignItems={"center"}
       justifyContent={"center"}
-      minHeight={"calc(100vh - 175px)"}
+      minHeight={"100vh"}
+      marginBottom={"100px"}
+      background={"red"}
+      gap={"30px"}
     >
-      <div className={Style.trapezoid}>
-        <img className={Style.box} src={me} alt="" />
-      </div>
-      <Box>
-        <h1 className={Style.name}>
-          Hi, I'm{" "}
-          <span
-            style={{
-              background: info.gradient,
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-            }}
+      <Box
+        flexDirection={{ xs: "column", md: "row" }}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className={Style.trapezoid}>
+          <img className={Style.box} src={me} alt="" />
+        </div>
+        <Box>
+          <h1 className={Style.name}>
+            Hi, I'm{" "}
+            <span
+              style={{
+                background: info.gradient,
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+            >
+              {info.firstName}
+            </span>
+            <span className={Style.hand}>🏴‍☠️</span>
+          </h1>
+          <h2>I'm Frontend Developer.</h2>
+          <Box component={"ul"} p={"0.8rem"}>
+            {info.miniBio.map((bio, index) => (
+              <EmojiBullet
+                key={index}
+                emoji={bio.emoji}
+                text={bio.text}
+                link={bio.link}
+              />
+            ))}
+          </Box>
+          <Box
+            display={"flex"}
+            gap={"1.5rem"}
+            justifyContent={"center"}
+            fontSize={{ xs: "2rem", md: "2.5rem" }}
           >
-            {info.firstName}
-          </span>
-          <span className={Style.hand}>🏴‍☠️</span>
-        </h1>
-        <h2>I'm Frontend Developer.</h2>
-        <Box component={"ul"} p={"0.8rem"}>
-          {info.miniBio.map((bio, index) => (
-            <EmojiBullet
-              key={index}
-              emoji={bio.emoji}
-              text={bio.text}
-              link={bio.link}
-            />
-          ))}
-        </Box>
-        <Box
-          display={"flex"}
-          gap={"1.5rem"}
-          justifyContent={"center"}
-          fontSize={{ xs: "2rem", md: "2.5rem" }}
-        >
-          {info.socials.map((social, index) => (
-            <SocialIcon
-              key={index}
-              link={social.link}
-              icon={social.icon}
-              label={social.label}
-            />
-          ))}
+            {info.socials.map((social, index) => (
+              <SocialIcon
+                key={index}
+                link={social.link}
+                icon={social.icon}
+                label={social.label}
+              />
+            ))}
+          </Box>
         </Box>
       </Box>
-      <div className={Style.qoutesMain}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi porro
-        hic blanditiis qui eaque provident laboriosam recusandae consectetur
-        accusantium saepe.
-      </div>
+      <div className={Style.qoutesMain}>"{quote.content}"</div>
     </Box>
   );
 }
